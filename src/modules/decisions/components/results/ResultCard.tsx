93afbe28fe9aba40707dcafd26a5cfb41b28597a
Jere Ljungberg <jere@mysteerio.fi>
2022-02-16 10:26:07 +0200

    Move ResultCard styles to .module.scss-file

--- src/modules/decisions/components/results/ResultCard.tsx
+++ src/modules/decisions/components/results/ResultCard.tsx
@@ -4,7 +4,8 @@ import { format } from 'date-fns';
 
 import useDepartmentClasses from '../../../../hooks/useDepartmentClasses';
 
-import './ResultCard.scss';
+import style from './ResultCard.module.scss';
+import classNames from 'classnames';
 
 type Props = {
   category: string,
@@ -36,37 +37,38 @@ const ResultCard = ({category, color_class, date, href, policymaker, subject, _s
 
   return (
     <div 
-      className='search-result__wrapper'
+      className={style.ResultCard}
       onClick={handleClick}
       onKeyPress={handleKeyPress}
       tabIndex={0}
     >
-      <div className='search-result__label' style={{backgroundColor: colorClass}}>
+      <div className={style.ResultCard__label} style={{backgroundColor: colorClass}}>
         { policymaker }
       </div>
-      <div className='search-result__container'>
-        <div className='search-result__header'>
-          {formattedDate &&
-            <div className='search-result__date'>
-              { formattedDate }
-            </div>
-          }
+      <div className={style.ResultCard__container}>
+        <div>
+          <div className={style.ResultCard__date}>
+            { formattedDate }
+          </div>
         </div>
-        <div className='search-result__title'>
+        <div className={style.ResultCard__title}>
           {process.env.REACT_APP_DEVELOPER_MODE &&
             <span style={{color: 'red'}}>{ _score }</span>
           }
           <h2>{ subject }</h2>
         </div>
       </div>
-      <div className='search-result__footer'>
+      <div className={style.ResultCard__footer}>
           {
             category &&
-              <div className='search-result__tag-container paatokset-tag-container'>
-                <span className='search-tag'>{ category }</span>
+              <div className={classNames(
+                style.ResultCard__tags,
+                'paatokset-tag-container'
+              )}>
+                <span className={style['search-tag']}>{ category }</span>
               </div>
           }
-          <div className='search-result__issue-link'>
+          <div className={style['ResultCard__issue-link']}>
               <IconArrowRight size={'l'}/>
           </div>
         </div>
